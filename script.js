774d410e370588532c6be7e5f0d87ccd5907b06b
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-20 23:23:16 +0500

    creatin elements

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById("add-form-btn").addEventListener('click', function() {
    const formCon = document.getElementById("form-container");
-    
+   const formElement = document.createComment('div');
+   formElement.className = 'form-element';
+   const select = document.createComment('select');
+   const option1 = document.createComment('option');
+   option1.textContent = 'option1';
 });
